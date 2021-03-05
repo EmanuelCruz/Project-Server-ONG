42cdbdc6902c6ac068b410402353dfb7a780f53a
DESKTOP-7E8ENMV\moish <moises.dev08@gmail.com>
2021-03-05 14:28:07 -0300

    feature/login consts created

--- constant/const.js
+++ constant/const.js
@@ -1,8 +1,10 @@
 module.exports = {
-    // GET organizacion public
-    url_org: '/:id/public',
-    // SUCCESS code
-    code_success: 200,
-    // FAILURE code
-    code_failure: 500
+  // GET organizacion public
+  url_org: "/:id/public",
+  // POST login with email and password
+  url_auth_login: "/login",
+  // SUCCESS code
+  code_success: 200,
+  // FAILURE code
+  code_failure: 500,
 };
