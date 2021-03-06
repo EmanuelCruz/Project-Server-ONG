8797462acf302baf4e38f59000eb0d809678a3d3
adway94 <alejandromachulsky_1o@hotmail.com>
2021-03-06 09:49:29 -0300

    New Feature Mailing basic functionality

--- constant/const.js
+++ constant/const.js
@@ -1,8 +1,14 @@
 module.exports = {
-    // GET organizacion public
-    url_org: '/:id/public',
-    // SUCCESS code
-    code_success: 200,
-    // FAILURE code
-    code_failure: 500
+  // GET organizacion public
+  url_org: "/:id/public",
+  // SUCCESS code
+  code_success: 200,
+  // FAILURE code
+  code_failure: 500,
+
+  //Mail API KEY
+  SENGRID_API_KEY_TESTING:
+    "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",
+  SENGRID_API_KEY:
+    "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",
 };
