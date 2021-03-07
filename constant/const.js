578569da48bf70c5fb0f9a5c48a9c29b337df767
Machalky <alejandromachulsky_1o@hotmail.com>
2021-03-07 10:55:47 -0300

    New Feature Mailing Service

--- constant/const.js
+++ constant/const.js
@@ -11,4 +11,20 @@ module.exports = {
     "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",
   SENGRID_API_KEY:
     "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",
+
+  //Mailing data and fields
+  COMPANY_MAIL: "", //Replace with validated mail from sengrid
+  COMPANY_MAIL_TESTING: "adway94@gmail.com", //Only for testing porpuse
+
+  //Register Mail
+  MAIL_REGISTER_SUBJECT: "Regisro exitoso",
+  MAIL_REGISTER_TEXT: "Su registro fue exitoso con los siguientes datos",
+
+  //Contact Form mail
+  MAIL_CONTACT_FORM_SUBJECT: "Recibimos su mensaje correctamente",
+  MAIL_CONTACT_FORM_TEXT: "Nos estaremos comunicando a la brevedad",
+
+  //Password Change Mail
+  MAIL_PASSWORD_CHANGE_SUBJECT: "Su contraseña fue cambiada con exito",
+  MAIL_PASSWORD_CHANGE_TEXT: "Su nueva contraseña es ",
 };
--- services/mailing/mailApi.js
+++ services/mailing/mailApi.js
@@ -1,27 +1,17 @@
-//Se enviarán mails en diferentes situaciones (al completar el formulario de contacto, registrarse en el sitio, etc). Para ello, es necesario crear un servicio que permita enviarlos. Utilizaremos la API de sendgrid.Inicialmente, las tareas son:
-
-// -Instalar librería que permita enviar emails utilizando sendgrid
-
-// -Crear método que reciba como parámetro una dirección de email y realice un envío a la misma.
-
 const constant = require("../../constant/const");
 const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(constant.SENGRID_API_KEY_TESTING);
+sgMail.setApiKey(constant.SENGRID_API_KEY);
 
-const msg = {
-  to: "alejandromachulsky_1o@hotmail.com", // Change to your recipient
-  from: "adway94@gmail.com", // Change to your verified sender
-  subject: "Sengrid esta bueno papa, MIrA!",
-  text: "and easy to do anywhere, even with Node.js",
-  html: "<h1><strong>Mail enivado</strong><h1>",
-};
+function sendMail(msg) {
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log("Email sent");
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+module.exports = sendMail;
