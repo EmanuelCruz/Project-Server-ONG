2a4c96bbffd5f1de4bc7045a721097fd01bd6fe9
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-08 18:32:25 -0300

    Add try catch

--- controllers/validateToken.js
+++ controllers/validateToken.js
@@ -1,22 +1,26 @@
 const jwt = require("jsonwebtoken");
 
 async function validateTokent(req, res, next) {
-    const token = await req.headers["authorization"];
+    try {
+        const token = await req.headers["authorization"];
 
-    if (!token) {
-        return res.status(401).json({
-            auth: false,
-            message: "no token provided",
-        });
+        if (!token) {
+            return res.status(401).json({
+                auth: false,
+                message: "no token provided",
+            });
+        }
+
+        const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
+        const { email, password } = decoded.data;
+
+        req.email = email;
+        req.password = password;
+
+        next();
+    } catch (err) {
+        res.json(err);
     }
-
-    const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
-    const { email, password } = decoded.data;
-
-    req.email = email;
-    req.password = password;
-
-    next();
 }
 
 module.exports = validateTokent;
