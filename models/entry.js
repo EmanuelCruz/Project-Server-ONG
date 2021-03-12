539318ced6c41c8d129bf25f83122ad1bfb3da7d
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-12 12:29:18 -0300

    Fix: Wrong alias

--- models/entry.js
+++ models/entry.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Entry.belongsTo(models.Categories, {as: 'Categories'});
+      Entry.belongsTo(models.Categories, {as: 'category'});
     }
   };
   Entry.init({
