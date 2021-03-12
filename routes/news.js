204f4dbd3e52caa0e1ca42830ea0b48a6f1036cf
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-12 12:37:40 -0300

    Feat: Add new path for news by id

---
+++ routes/news.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+var newsController = require("../controllers/news");
+var consts = require("../constant/const");
+
+/* GET news by ID. */
+router.get(consts.URL_NEWS_ID, newsController.getNewsById);
+
+module.exports = router;
