e6032ef84b7bbf4da1dbbca08bf3495b6d6705ae
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-16 20:30:08 -0300

    Feat: Adding constants for route and query of activities

--- constant/const.js
+++ constant/const.js
@@ -1,63 +1,72 @@
 const { body } = require("express-validator");
 
 module.exports = {
-  // GET organizacion public
-  url_org: "/:id/public",
-  // POST login with email and password
-  url_auth_login: "/login",
-  // GET all users
-  url_users: "/users",
-  // SUCCESS code
-  code_success: 200,
-  // FAILURE code
-  code_failure: 500,
-  //FAILURE CODE 404
-  CODE_FAILURE_404: 404,
-  //USER NOT FOUND
-  NOT_FOUND_USER: "User not found",
-  //** This string is used for validate email and password fields when the user login */
-  loginValuesFieldValidator: [
-    body("email").exists().isEmail().withMessage("Invalid email or password"),
-    body("password")
-      .isLength({ min: 6 })
-      .withMessage("Invalid email or password"),
-  ],
+    // GET organizacion public
+    url_org: "/:id/public",
+    // POST login with email and password
+    url_auth_login: "/login",
+    // GET all users
+    url_users: "/users",
+    // SUCCESS code
+    code_success: 200,
+    // FAILURE code
+    code_failure: 500,
+    //FAILURE CODE 404
+    CODE_FAILURE_404: 404,
+    //POST Success
+    POST_SUCCESS: 201,
+    //POST bad request
+    POST_FAILURE: 400,
+    //USER NOT FOUND
+    NOT_FOUND_USER: "User not found",
+    //** This string is used for validate email and password fields when the user login */
+    loginValuesFieldValidator: [
+        body("email")
+            .exists()
+            .isEmail()
+            .withMessage("Invalid email or password"),
+        body("password")
+            .isLength({ min: 6 })
+            .withMessage("Invalid email or password"),
+    ],
 
-  //Mail API KEY
-  SENGRID_API_KEY_TESTING:
-    "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",
-  SENGRID_API_KEY:
-    "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",
+    //Mail API KEY
+    SENGRID_API_KEY_TESTING:
+        "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",
+    SENGRID_API_KEY:
+        "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",
 
-  //Mailing data and fields
-  COMPANY_MAIL: "", //Replace with validated mail from sengrid
-  COMPANY_MAIL_TESTING: "adway94@gmail.com", //Only for testing porpuse
+    //Mailing data and fields
+    COMPANY_MAIL: "", //Replace with validated mail from sengrid
+    COMPANY_MAIL_TESTING: "adway94@gmail.com", //Only for testing porpuse
 
-  //Register Mail
-  MAIL_REGISTER_SUBJECT: "Regisro exitoso",
-  MAIL_REGISTER_TEXT: "Su registro fue exitoso con los siguientes datos",
+    //Register Mail
+    MAIL_REGISTER_SUBJECT: "Regisro exitoso",
+    MAIL_REGISTER_TEXT: "Su registro fue exitoso con los siguientes datos",
 
-  //Contact Form mail
-  MAIL_CONTACT_FORM_SUBJECT: "Recibimos su mensaje correctamente",
-  MAIL_CONTACT_FORM_TEXT: "Nos estaremos comunicando a la brevedad",
+    //Contact Form mail
+    MAIL_CONTACT_FORM_SUBJECT: "Recibimos su mensaje correctamente",
+    MAIL_CONTACT_FORM_TEXT: "Nos estaremos comunicando a la brevedad",
 
-  //Password Change Mail
-  MAIL_PASSWORD_CHANGE_SUBJECT: "Su contraseña fue cambiada con exito",
-  MAIL_PASSWORD_CHANGE_TEXT: "Su nueva contraseña es ",
+    //Password Change Mail
+    MAIL_PASSWORD_CHANGE_SUBJECT: "Su contraseña fue cambiada con exito",
+    MAIL_PASSWORD_CHANGE_TEXT: "Su nueva contraseña es ",
 
-  // User is not an admin:
-  USER_IS_NOT_AN_ADMIN: "El usuario no es administrador",
-  //News Route
-  URL_NEWS_ID: "/:id",
-  MESSAGE_NOT_FOUND: "News Not found",
-  ARRAY_ENPTY: 0,
-  //Authenticated user:
-  // Authorization user token
-  AUTHORIZATION: "authorization",
-  TOKEN_IS_NOT_PROVIDED: "no token provided",
-  // User Atrributes
-  FIRST_NAME: "firstName",
-  LAST_NAME: "lastName",
-  EMAIL: "email",
-  IMAGE: "image",
+    // User is not an admin:
+    USER_IS_NOT_AN_ADMIN: "El usuario no es administrador",
+    //News Route
+    URL_NEWS_ID: "/:id",
+    MESSAGE_NOT_FOUND: "News Not found",
+    ARRAY_ENPTY: 0,
+    //Authenticated user:
+    // Authorization user token
+    AUTHORIZATION: "authorization",
+    TOKEN_IS_NOT_PROVIDED: "no token provided",
+    // User Atrributes
+    FIRST_NAME: "firstName",
+    LAST_NAME: "lastName",
+    EMAIL: "email",
+    IMAGE: "image",
+    //Activities
+    URL_POST_ACTIVITIES: "/",
 };
