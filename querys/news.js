d220c63a739ab85adb256dcd71cd96f030003062
jhosep98 <jhosep149@hotmail.com>
2021-03-17 16:51:47 -0300

    creation of the function to delete an entry of type news

--- querys/news.js
+++ querys/news.js
@@ -2,24 +2,33 @@ const db = require("../models");
 const Entry = db.Entry;
 
 exports.getEntry = async (entryId) => {
-    try {
-        const entry = await Entry.findAll({
-            where: { id: entryId, type: "news" },
-        });
-        return entry;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const entry = await Entry.findAll({
+      where: { id: entryId, type: "news" },
+    });
+    return entry;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 exports.getTypeNews = async (typeNews) => {
-    try {
-        const news = await Entry.findAll({
-            where: { type: typeNews },
-            attributes: ["name", "image", "createdAt"],
-        });
-        return news;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const news = await Entry.findAll({
+      where: { type: typeNews },
+      attributes: ["name", "image", "createdAt"],
+    });
+    return news;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.deleteNew = async (id) => {
+  try {
+    const entry = await Entry.destroy({ where: { id }, type: "news" });
+    return entry;
+  } catch (error) {
+    console.log(error);
+  }
 };
