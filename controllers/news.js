ec6f153f452d5fefc9a3032d20fd57c064211f1b
Machalky <alejandromachulsky_1o@hotmail.com>
2021-03-18 22:26:15 -0300

    New Feature News Update

--- controllers/news.js
+++ controllers/news.js
@@ -1,31 +1,61 @@
 const newsQuery = require("../querys/news");
 var consts = require("../constant/const");
+const updateEntry = require("../querys/news");
+const uploadImage = require("../services/aws/s3UploadImage");
 
 exports.getNews = (req, res, next) => {
-    newsQuery
-        .getTypeNews("news")
-        .then((org) => {
-            res.status(consts.code_success).send(org);
-        })
-        .catch((err) =>
-            res.status(consts.code_failure).send({ message: err.message })
-        );
+  newsQuery
+    .getTypeNews("news")
+    .then((org) => {
+      res.status(consts.code_success).send(org);
+    })
+    .catch((err) =>
+      res.status(consts.code_failure).send({ message: err.message })
+    );
 };
 
 exports.getNewsById = (req, res, next) => {
-    const entryId = req.params.id;
-    newsQuery
-        .getEntry(entryId)
-        .then((dataEntry) => {
-            if (dataEntry.length === consts.ARRAY_ENPTY) {
-                res.status(consts.code_success).send({
-                    message: consts.MESSAGE_NOT_FOUND,
-                });
-            } else {
-                res.status(consts.code_success).send(dataEntry);
-            }
+  const entryId = req.params.id;
+  newsQuery
+    .getEntry(entryId)
+    .then((dataEntry) => {
+      if (dataEntry.length === consts.ARRAY_ENPTY) {
+        res.status(consts.code_success).send({
+          message: consts.MESSAGE_NOT_FOUND,
+        });
+      } else {
+        res.status(consts.code_success).send(dataEntry);
+      }
+    })
+    .catch((err) =>
+      res.status(consts.code_failure).send({ message: err.message })
+    );
+};
+
+exports.updateNews = (req, res) => {
+  if (!req.params.id) {
+    res
+      .status(consts.FORBIDDEN_ACTION_CODE)
+      .send({ error: consts.NOT_FOUND_USER });
+  } else {
+    const news = {
+      name: req.body.name,
+      content: req.body.content,
+      categoryId: req.body.categoryId,
+    };
+    uploadImage(req, (img) => {
+      news["image"] = img;
+      newsQuery
+        .updateEntry(news, req.params.id)
+        .then((dataNews) => {
+          if (dataNews.length == consts.ARRAY_ENPTY) {
+            throw new Error(consts.NOT_FOUND_USER);
+          }
+          res.status(consts.code_success).json(dataNews);
         })
-        .catch((err) =>
-            res.status(consts.code_failure).send({ message: err.message })
-        );
+        .catch((err) => {
+          res.status(consts.code_failure).send({ Error: err.message });
+        });
+    });
+  }
 };
--- routes/news.js
+++ routes/news.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-
+const multer = require("multer");
 var newsController = require("../controllers/news");
 var consts = require("../constant/const");
 
@@ -8,5 +8,12 @@ var consts = require("../constant/const");
 router.get("/", newsController.getNews);
 /* GET news by ID. */
 router.get(consts.URL_NEWS_ID, newsController.getNewsById);
+
+/*Update news*/
+router.patch(
+  "/:id",
+  multer(consts.MULTER_DESTINATION_PARAMS).single(consts.MULTER_KEY_NAME),
+  newsController.updateNews
+);
 
 module.exports = router;
