3dbb08070d9bd2a075f5a3331055d8bc51f44d64
DESKTOP-7E8ENMV\moish <moises.dev08@gmail.com>
2021-03-21 13:04:06 -0300

    feature/contacts fields added to organizations query

--- querys/organizations.js
+++ querys/organizations.js
@@ -4,6 +4,15 @@ const Organization = db.Organization;
 exports.getOrganization = (orgId) => {
   return Organization.findOne({
     where: { id: orgId },
-    attributes: ["name", "image", "phone", "address", "welcomeText"],
+    attributes: [
+      "name",
+      "image",
+      "phone",
+      "address",
+      "welcomeText",
+      "contactFacebook",
+      "contactLinkedin",
+      "contactTwitter",
+    ],
   });
 };
