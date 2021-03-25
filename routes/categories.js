db0fc8ef0a6926535bd0a864c6c3b450f613beb5
DESKTOP-7E8ENMV\moish <moises.dev08@gmail.com>
2021-03-25 15:17:52 -0300

    feature/categoriesDelete controller created

--- routes/categories.js
+++ routes/categories.js
@@ -6,4 +6,10 @@ const consts = require("../constant/const");
 // Update categories endpoint
 router.put(consts.URL_CATEGORIES_UPDATE, categoriesController.categoriesUpdate);
 
+// Delete categories endpoint
+router.delete(
+  consts.URL_CATEGORIES_DELETE,
+  categoriesController.categoriesDelete
+);
+
 module.exports = router;
